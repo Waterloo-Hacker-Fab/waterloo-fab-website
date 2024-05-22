6d49514e0b17f34e979d8868fc64f79b05d07d45
Abhay <rattiabhay@gmail.com>
2024-05-22 18:17:33 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import cooking from './img/cooking2.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Waterloo Hacker Fab</h1>
+      <h3>🚧 fab under construction 🚧</h3>
+      <p>check back soon for updates!</p>
+      <img src={cooking} className="img" alt="logo" />
     </div>
   );
 }
