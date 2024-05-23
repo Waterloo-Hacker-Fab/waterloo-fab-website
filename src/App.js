535de359326025bf401a88e1bf44a7995393ceb0
Abhay <rattiabhay@gmail.com>
2024-05-23 19:57:46 -0400

    typing effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import cooking from './img/cooking2.jpg';
+import React, { useState, useEffect } from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import cooking from './img/hacker_fab_logo.png';
 import './App.css';
 
 function App() {
+  const [showH3, setShowH3] = useState(false);
+  const [showP, setShowP] = useState(false);
+
+  useEffect(() => {
+    // Delay for showing h3 after h1 finishes
+    const h3Timer = setTimeout(() => setShowH3(true), 2000); // Adjust timing as needed
+    return () => clearTimeout(h3Timer);
+  }, []);
+
+  useEffect(() => {
+    if (showH3) {
+      // Delay for showing p after h3 finishes
+      const pTimer = setTimeout(() => setShowP(true), 3000); // Adjust timing as needed
+      return () => clearTimeout(pTimer);
+    }
+  }, [showH3]);
+
   return (
     <div className="main">
-      <h1>Waterloo Hacker Fab</h1>
-      <h3>🚧 fab under construction 🚧</h3>
-      <p>check back soon for updates!</p>
       <img src={cooking} className="img" alt="logo" />
+      <h1>
+        <Typewriter
+          words={['Waterloo Hacker Fab']}
+          loop={1}
+          typeSpeed={50}
+          cursor
+          cursorStyle='_'
+        />
+      </h1>
+      {showH3 && (
+        <h3>
+          <Typewriter
+            words={["We're currently under construction 🚧"]}
+            loop={1}
+            typeSpeed={50}
+            cursor
+            cursorStyle='_'
+          />
+        </h3>
+      )}
+      {showP && (
+        <p>
+          <Typewriter
+            words={['Please check back soon for updates!']}
+            loop={1}
+            typeSpeed={50}
+            cursor
+            cursorStyle='_'
+          />
+        </p>
+      )}
     </div>
   );
 }
